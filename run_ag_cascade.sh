997135c8da9b6573a4b5495e18d959dff06acc34
Jiaying <jylu@amazon.com>
2022-07-21 00:57:24 +0000

    add aws configures

--- run_ag_cascade.sh
+++ run_ag_cascade.sh
@@ -1,3 +1,7 @@
 # Jul 20
 # validation
-python runbenchmark.py autogluon_v0.5.1 validation -u user_dir
+# python runbenchmark.py autogluon_v0.5.1 validation -u user_dir
+
+# POC with infer_limit
+# python runbenchmark.py autogluon_v0.5.1_high_il0.001 example 1h8c -u user_dir
+python runbenchmark.py autogluon_v0.5.1_high_il0.001 validation 1h8c -u user_dir -m aws -p 2
