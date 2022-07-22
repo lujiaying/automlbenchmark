93fd9538bdddb0f28393d5416c3860ed8383b427
Jiaying Lu <jylu@amazon.com>
2022-07-22 07:07:09 +0000

    add cumstom resource files

--- run_ag_cascade.sh
+++ run_ag_cascade.sh
@@ -3,5 +3,5 @@
 # python runbenchmark.py autogluon_v0.5.1 validation -u user_dir
 
 # POC with infer_limit
-# python runbenchmark.py autogluon_v0.5.1_high_il0.001 example 1h8c -u user_dir
-python runbenchmark.py autogluon_v0.5.1_high_il0.001 validation 1h8c -u user_dir -m aws -p 2
+python runbenchmark.py autogluon_v0.5.1_high_il0.001 example 1h8c -u user_dir -m aws
+# python runbenchmark.py autogluon_v0.5.1_high_il0.001 validation 1h8c -u user_dir -m aws -p 2
