6ae22cb8d8c32c31936b35cefafb0b78b2bb9d1a
Jiaying Lu <jylu@amazon.com>
2022-07-25 05:58:08 +0000

    add Jul24 failed tasks for multiclass

---
+++ ag_cascade_report/analysis_ag_cascade_benchmark.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def show_failed_tasks(df: pd.DataFrame):
+    df = df[df.logloss.isnull()][['id', 'task', 'fold', 'info']]
+    print(df)
+
+if __name__ == '__main__':
+    multiclass_ag_il1ms_path = '/home/ec2-user/automlbenchmark/results/autogluon_v0_5_1_high_il0_001.openml_bench_271-multiclass.1h8c.aws.20220724T190434/scores/results.csv'
+     
+    df = pd.read_csv(multiclass_ag_il1ms_path)
+    show_failed_tasks(df)
