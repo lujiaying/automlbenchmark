f2d9515912673866cf206a43e37e9a9f9404d194
Jiaying Lu <jylu@amazon.com>
2022-08-02 23:29:23 +0000

    for re-run failed 5 datasets

--- run_ag_cascade.sh
+++ run_ag_cascade.sh
@@ -3,6 +3,11 @@
 # python runbenchmark.py AGv053_Jul30_high example 1h8c_gp3 -f 0 -m aws -p 1
 
 # Jul 30
-#python runbenchmark.py AGv053_Jul30_high_il0.002 ag 1h8c_gp3 -m aws -p 1000
-python runbenchmark.py AGv053_Jul30_high ag 1h8c_gp3 -m aws -p 1000
-python runbenchmark.py AGv053_Jul30_high_il0.005 ag 1h8c_gp3 -m aws -p 1000
+# python runbenchmark.py AGv053_Jul30_high_il0.002 ag 1h8c_gp3 -m aws -p 1000
+# python runbenchmark.py AGv053_Jul30_high ag 1h8c_gp3 -m aws -p 1000
+# python runbenchmark.py AGv053_Jul30_high_il0.005 ag 1h8c_gp3 -m aws -p 1000
+
+# Aug 2: re-run failed
+python runbenchmark.py AGv053_Jul30_high_il0.0002 ag 1h8c_gp3 -m aws -p 50
+python runbenchmark.py AGv053_Jul30_high_il0.002 ag 1h8c_gp3 -m aws -p 50
+python runbenchmark.py AGv053_Jul30_high_il0.005 ag 1h8c_gp3 -m aws -p 50
