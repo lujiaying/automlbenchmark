a1a301dfa8ca74765b8b15e3e3118d54bfa906c9
Jiaying Lu <jylu@amazon.com>
2022-08-03 02:32:27 +0000

    add run failed scripts

--- run_ag_cascade.sh
+++ run_ag_cascade.sh
@@ -8,6 +8,5 @@
 # python runbenchmark.py AGv053_Jul30_high_il0.005 ag 1h8c_gp3 -m aws -p 1000
 
 # Aug 2: re-run failed
-python runbenchmark.py AGv053_Jul30_high_il0.0002 ag 1h8c_gp3 -m aws -p 50
-python runbenchmark.py AGv053_Jul30_high_il0.002 ag 1h8c_gp3 -m aws -p 50
-python runbenchmark.py AGv053_Jul30_high_il0.005 ag 1h8c_gp3 -m aws -p 50
+python runbenchmark.py AGv053_Jul30_high_il0.002 ag_0730_failed_5_datasets 1h8c_gp3 -m aws -p 50
+python runbenchmark.py AGv053_Jul30_high_il0.005 ag_0730_failed_5_datasets 1h8c_gp3 -m aws -p 50
